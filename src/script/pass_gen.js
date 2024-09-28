932e700e463b46cdbf692425ec71309d92e5f846
royaly <81520338+royaly-dev@users.noreply.github.com>
2024-09-28 16:02:29 +0200

    added pass gen

--- src/script/pass_gen.js
+++ src/script/pass_gen.js
@@ -1,1 +1,26 @@
-// script to generate random password
+function genererMotDePasse(longueur) {
+
+    const min = "abcdefghijklmnopqrstuvwxyz";
+    const maj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const number = "0123456789";
+    const spec = "!@#$%:?/.*-_";
+
+    let pass = [
+        min[Math.floor(Math.random() * min.length)],
+        maj[Math.floor(Math.random() * maj.length)],
+        number[Math.floor(Math.random() * number.length)],
+        spec[Math.floor(Math.random() * spec.length)]
+    ];
+
+    const all = min + maj + number + spec;
+    for (let i = 4; i < longueur; i++) {
+        pass.push(all[Math.floor(Math.random() * all.length)]);
+    }
+
+    pass = pass.sort(() => Math.random() - 0.5);
+
+    return pass.join('');
+}
+
+const newpass = genererMotDePasse(20);
+console.log(newpass);
