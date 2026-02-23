6a1d1b59af3eed9021745476c8b5fc3f0a78d688
royaly-dev <clemgc33@gmail.com>
2026-02-23 16:23:53 +0100

    added syncData type

--- src/types/Data.ts
+++ src/types/Data.ts
@@ -1,4 +1,7 @@
-export type Data = {password: {id: string, password: string, url: string, mail: string}[], opt: {id: string, key: string, name: string, provider: string}[]}
-export type PasswordData = {id: string, password: string, url: string, mail: string}
-export type AccountData = {id: string, mail: string}
-export type OptData = {id: string, key: string, name: string, provider: string}
+export type Data = { password: { id: string, password: string, url: string, mail: string }[], opt: { id: string, key: string, name: string, provider: string }[] }
+export type PasswordData = { id: string, password: string, url: string, mail: string }
+export type AccountData = { id: string, mail: string }
+export type OptData = { id: string, key: string, name: string, provider: string }
+
+export type syncDevice = { syncKey: string, data: { syncKey: string, lastSync: number, name: string }[], lastSync: number, status: boolean }
+export type syncData = { syncKey: string, lastSync: number, name: string }
