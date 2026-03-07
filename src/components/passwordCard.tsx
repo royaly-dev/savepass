6040068024fcd5c052797e33e10ba2c638a4c3c9
royaly-dev <clemgc33@gmail.com>
2026-03-07 13:47:01 +0100

    fixed problem while favicon not display

--- src/components/passwordCard.tsx
+++ src/components/passwordCard.tsx
@@ -24,35 +24,35 @@ export default function PasswordCard(props: { data: PasswordData, requestEdit(da
             <CardContent className="flex justify-between items-center gap-2 px-0">
                 <div className="flex justify-center items-center gap-2">
                     {errorLoadingImage
-                        ? <Globe size={24} color="#0769e1e8" className="p-3 box-content bg-[#0769e152] rounded-md"/>
+                        ? <Globe size={24} color="#0769e1e8" className="p-3 box-content bg-[#0769e152] rounded-md" />
                         : <div className="p-3 box-content rounded-md relative overflow-hidden">
-                            <img style={{height: 24, width: 24, borderRadius: "100%"}} src={"https://"+(new URL(props.data.url).hostname)+"/favicon.ico"} onError={() => {setErrorLoadingImage(true)}} className="z-10 relative" />
-                            <img src={"https://"+(new URL(props.data.url).hostname)+"/favicon.ico"} onError={() => {setErrorLoadingImage(true)}} className="absolute w-full top-0 left-0 z-0 rounded-md blur-md" />
+                            <img style={{ height: 24, width: 24, borderRadius: "100%" }} src={"https://www.google.com/s2/favicons?domain=" + new URL(props.data.url)} onError={() => { setErrorLoadingImage(true) }} className="z-10 relative" />
+                            <img src={"https://www.google.com/s2/favicons?domain=" + new URL(props.data.url)} onError={() => { setErrorLoadingImage(true) }} className="absolute w-full top-0 left-0 z-0 rounded-md blur-md" />
                         </div>
                     }
                     <div className="flex justify-center items-start flex-col">
-                        <div onClick={() => {openLink(props.data.url)}} className="flex justify-center items-center group cursor-pointer">
+                        <div onClick={() => { openLink(props.data.url) }} className="flex justify-center items-center group cursor-pointer">
                             <span>{new URL(props.data.url).host}</span>
                             <ArrowUpRight className="group-hover:scale-125 group-hover:-translate-y-0.5 transition-all duration-200" size={24} />
                         </div>
                         <div className="text-muted-foreground flex justify-center items-center gap-1">
-                            <span onClick={() => {copyToClipBoard(props.data.mail)}} className="p-0.5 px-1 hover:bg-muted-foreground hover:text-white rounded-sm cursor-pointer transition-all duration-300">{props.data.mail}</span>
-                            <span onClick={() => {copyToClipBoard(props.data.password)}} className="p-0.5 px-1 hover:bg-muted-foreground hover:text-white rounded-sm cursor-pointer transition-all duration-300">{showpass ? props.data.password : "********"}</span>
+                            <span onClick={() => { copyToClipBoard(props.data.mail) }} className="p-0.5 px-1 hover:bg-muted-foreground hover:text-white rounded-sm cursor-pointer transition-all duration-300">{props.data.mail}</span>
+                            <span onClick={() => { copyToClipBoard(props.data.password) }} className="p-0.5 px-1 hover:bg-muted-foreground hover:text-white rounded-sm cursor-pointer transition-all duration-300">{showpass ? props.data.password : "********"}</span>
                         </div>
                     </div>
                 </div>
                 <div className="flex justify-center items-center gap-2.5">
                     {
                         !showpass
-                        ? <Eye onClick={() => {setShowpass(true)}} size={18} className="cursor-pointer p-1 box-content hover:bg-muted-foreground/50 rounded-md duration-300 transition-all" />
-                        : <EyeOff onClick={() => {setShowpass(false)}} size={18} className="cursor-pointer p-1 box-content hover:bg-muted-foreground/50 rounded-md duration-300 transition-all" />
+                            ? <Eye onClick={() => { setShowpass(true) }} size={18} className="cursor-pointer p-1 box-content hover:bg-muted-foreground/50 rounded-md duration-300 transition-all" />
+                            : <EyeOff onClick={() => { setShowpass(false) }} size={18} className="cursor-pointer p-1 box-content hover:bg-muted-foreground/50 rounded-md duration-300 transition-all" />
                     }
-                    <Copy onClick={() => {copyToClipBoard(props.data.password)}} size={18} className="cursor-pointer p-1 box-content hover:bg-muted-foreground/50 rounded-md duration-300 transition-all" />
+                    <Copy onClick={() => { copyToClipBoard(props.data.password) }} size={18} className="cursor-pointer p-1 box-content hover:bg-muted-foreground/50 rounded-md duration-300 transition-all" />
                     <DropdownMenu>
                         <DropdownMenuTrigger><EllipsisVertical size={18} className="cursor-pointer p-1 box-content hover:bg-muted-foreground/50 rounded-md duration-300 transition-all" /></DropdownMenuTrigger>
                         <DropdownMenuContent>
-                            <DropdownMenuItem onClick={() => {props.requestEdit(props.data)}} variant="default">Modify</DropdownMenuItem>
-                            <DropdownMenuItem onClick={() => {props.requestDelete(props.data)}} variant="destructive">Delete</DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => { props.requestEdit(props.data) }} variant="default">Modify</DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => { props.requestDelete(props.data) }} variant="destructive">Delete</DropdownMenuItem>
                         </DropdownMenuContent>
                     </DropdownMenu>
                 </div>
