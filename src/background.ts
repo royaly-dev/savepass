b9b93930aeebfdfe34788f187cf50c4da2b5aaff
royaly-dev <clemgc33@gmail.com>
2026-04-06 21:09:16 +0200

    make background and content communicate and added base for the popup

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,41 @@
 console.log("background scritp launched !")
 
-chrome.runtime.onMessage.addListener(async (message: string, sender: chrome.runtime.MessageSender) => {
-    console.log(message)
-    console.log("fetch")
-    const t = await fetch("http://localhost:5263/setupSync")
-    console.log("fetched")
-    console.log(t.status)
+chrome.runtime.onMessage.addListener((message: string, sender: chrome.runtime.MessageSender, sendResponse: (responce: string) => void) => {
+
+    const arg = message.split("_")
+    switch (arg[1]) {
+        case "status":
+
+            break;
+
+        case "checkpass":
+            checkService(sendResponse)
+            break;
+
+        default:
+            break;
+    }
+
+    return true
 })
+
+const StatusService = async (sendResponse: (responce: string) => void) => {
+    const status = await fetch("http://localhost:5263/status").catch((err) => { console.log(err); return null })
+
+    if (status?.status === 200) {
+        sendResponse("background_status_succes")
+    } else {
+        sendResponse("background_status_error")
+    }
+}
+
+const checkService = async (sendResponse: (responce: string) => void) => {
+    const check = await fetch("http://localhost:5263/check").catch((err) => { console.log(err); return null })
+
+    if (check?.status === 200) {
+        await chrome.action.openPopup()
+        sendResponse("background_checkpass_succes")
+    } else {
+        sendResponse("background_checkpass_error")
+    }
+}
--- src/content.ts
+++ src/content.ts
@@ -3,18 +3,30 @@ console.log("content loaded to this page !")
 async function checkFrom() {
     const inputs = document.querySelectorAll("input")
 
-    const hasPassAndEmail = { email: false, password: false }
+    const hasPassAndEmail: {
+        email: boolean
+        password: boolean
+        inputElementEmail: HTMLInputElement | null
+        inputElementPassword: HTMLInputElement | null
+    } = { email: false, password: false, inputElementEmail: null, inputElementPassword: null }
 
     for (const input of inputs) {
         if (input.type === "password") {
             hasPassAndEmail.password = true
+            hasPassAndEmail.inputElementPassword = input
+            input.style.borderColor = "red"
         } else if (input.type === "email" || input.type === "text" && (input.id === "user" || input.id === "username" || input.autocomplete === "username")) {
             hasPassAndEmail.email = true
+            hasPassAndEmail.inputElementEmail = input
+            input.style.borderColor = "red"
         }
     }
 
     if (hasPassAndEmail.email && hasPassAndEmail.password) {
-        chrome.runtime.sendMessage("checkpass_" + location.host)
+        const req = await chrome.runtime.sendMessage("background_checkpass_" + location.host)
+        if (req.split("_")[2] === "succes") {
+            console.log("succes")
+        }
     }
 }
 
